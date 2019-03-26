e946ad9c2912e38f43a471920d495624ff2f8933
Shuchen Zhu <shuchenzhu@ShuchentekiMacBook-Pro.local>
2019-03-26 13:37:16 -0400

    d

---
+++ short_range_interaction/error_1.py
@@ -0,0 +1,58 @@
+from numpy import *
+from matplotlib import pylab as plt
+import math as math
+
+def evolution_error_1(r,n):
+    t = n
+    s=10
+    p=4
+    gamma = 4
+    k = 2
+    p_k = 1/float(4-4**(1/(2*k-1)))
+    mu = max(abs(p_k)/2,abs(1-4*p_k)/2)
+    first_factor = floor(n/2)*gamma*mu
+    third_factor = t**(p+1)/float(math.factorial(p+1)*r**(p))
+    total_sum = 0
+    for j in range(1,s+1):
+        running_sum = 0
+        for i in range(1,j+1):
+            if i == 1:
+                running_sum += 2
+            elif i <= floor(n/2):
+                running_sum += i + i+1
+            else:
+                running_sum += 2*floor(n/2)
+        total_sum += first_factor*(2*gamma*mu*running_sum)**p * third_factor
+    return total_sum
+
+def evolution_error_2(r,n):
+    return evolution_error_1(r,n)
+
+def evolution_error_3(r,n):
+    t = n
+    s=10
+    p=4
+    gamma = 4
+    k = 2
+    p_k = 1/float(4-4**(1/(2*k-1)))
+    mu = max(abs(p_k)/2,abs(1-4*p_k)/2)
+    first_factor = n*gamma
+    third_factor = t**(p+1)/float(math.factorial(p+1)*r**(p))
+    running_sum = 0
+    for i in range(1,s+1):
+        if i == 1:
+            running_sum += 2
+        elif i <= floor(n/2):
+            running_sum += i + i+1
+        else:
+            running_sum += 2*floor(n/2)
+    total_sum = first_factor*(2*gamma*mu*running_sum)**p * third_factor
+    return total_sum
+
+total_error(r,n):
+    return 
+a=evolution_error_1(100000,100)
+b=evolution_error_3(100000,100)
+print(a)
+print(b)
+
