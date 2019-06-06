e639df3640db5c1af325acf0395a6029078ad8f0
Shuchen Zhu <shuchenzhu@ShuchentekiMacBook-Pro.local>
2019-06-06 17:04:01 -0400

    d

--- short_range_interaction/last_resort.py
+++ short_range_interaction/last_resort.py
@@ -265,22 +265,142 @@ def B_2(H,r):
     total_sum = total_sum*t**(p+1)/float(math.factorial(p+1)*r**(p))
     return total_sum
 
+def B_3(H,r):
+    total_sum = 0
+    for i in H:
+        running_sum = 0
+        current_support = [i]
+        second_matrix = zeros((4,4),dtype=complex)
+        if i-1 >0: 
+            current_support.append(i-1)
+            second_matrix += H_odd[i-1]
+        if i+1 <=n: 
+            current_support.append(i+1)
+            second_matrix += H_odd[i+1]
+        a_4 = a_coefficients[3]
+        b_sup_3 = 0
+        for c in range(3):
+            b_sup_3+= b_coefficients[c]
+        first_matrix = H_even[i]
+        inner_norm = abs(a_4*b_sup_3)*norm(matmul(second_matrix,first_matrix)-matmul(first_matrix,second_matrix),ord=2)
 
+        running_sum = inner_norm
+        total_sum += running_sum
+    total_sum = total_sum*t**(p+1)/float(math.factorial(p+1)*r**(p))
+    return total_sum
 
-n=10
-t=n
-h = generate_random_h(n)
-H_even = {}
-H_odd = {}
-H_even_array = []
-H_odd_array= []
-for i in range(1,n+1):
-    if i%2 == 0:
-        H_even[i] = matmul(kron(sigma_x,I),kron(I,sigma_x))+matmul(kron(sigma_y,I),kron(I,sigma_y))+matmul(kron(sigma_z,I),kron(I,sigma_z))+h[i-1]*kron(sigma_z,I)
-        H_even_array.append(i)
+def B_4(H,r):
+    total_sum = 0
+    for i in H:
+        running_sum = 0
+        current_support = [i]
+        second_matrix = zeros((4,4),dtype=complex)
+        if i-1 >0: 
+            current_support.append(i-1)
+            second_matrix += H_odd[i-1]
+        if i+1 <=n: 
+            current_support.append(i+1)
+            second_matrix += H_odd[i+1]
+        a_5 = a_coefficients[4]
+        b_sup_4 = 0
+        for c in range(4):
+            b_sup_4+= b_coefficients[c]
+        first_matrix = H_even[i]
+        inner_norm = abs(a_5*b_sup_4)*norm(matmul(second_matrix,first_matrix)-matmul(first_matrix,second_matrix),ord=2)
+
+        result,current_support = get_relevant_norms(current_support,H_even_array,H_odd_array,4)
+        running_sum += result*2
+        running_sum = inner_norm*running_sum**p
+        total_sum += running_sum
+
+    total_sum = total_sum*t**(p+1)/float(math.factorial(p+1)*r**(p))
+    return total_sum
+
+def B_5(H,r):
+    total_sum = 0
+    for i in H:
+        running_sum = 0
+        current_support = [i]
+        second_matrix = zeros((4,4),dtype=complex)
+        if i-1 >0: 
+            current_support.append(i-1)
+            second_matrix += H_odd[i-1]
+        if i+1 <=n: 
+            current_support.append(i+1)
+            second_matrix += H_odd[i+1]
+        a_6 = a_coefficients[5]
+        b_sup_5 = 0
+        for c in range(5):
+            b_sup_5+= b_coefficients[c]
+        first_matrix = H_even[i]
+        inner_norm = abs(a_6*b_sup_5)*norm(matmul(second_matrix,first_matrix)-matmul(first_matrix,second_matrix),ord=2)
+
+        result,current_support = get_relevant_norms(current_support,H_even_array,H_odd_array,5)
+        running_sum += result*2
+        result,current_support = get_relevant_norms(current_support,H_even_array,H_odd_array,4)
+        running_sum += result*2
+
+        running_sum = inner_norm*running_sum**p
+        total_sum += running_sum 
+    
+    total_sum = total_sum*t**(p+1)/float(math.factorial(p+1)*r**(p))
+    return total_sum
+
+def total_error(r,n):
+    A = A_1(H_odd,r)+A_2(H_odd,r)+A_3(H_odd,r)+A_4(H_odd,r)+A_5(H_odd,r)
+    B = B_1(H_even,r)+B_2(H_even,r)+B_3(H_even,r)+B_4(H_even,r)+B_5(H_even,r)
+    return A+B
+
+def find_r(n):
+    error = 10**(-3)
+    r = 1
+    previous_r = 0
+    while (total_error(r,n) > error):
+        previous_r = r
+        r = 2 * r
+    result = binary_search(previous_r,r,n,error)   
+    return result
+
+def binary_search(low,up,n,error):
+    if low + 1 >= up:
+        return int(up)
+    mid = ceil((low+up)/2)
+    if total_error(mid,n)< error:
+        return binary_search(low,mid,n,error)
     else:
-        H_odd[i] = matmul(kron(sigma_x,I),kron(I,sigma_x))+matmul(kron(sigma_y,I),kron(I,sigma_y))+matmul(kron(sigma_z,I),kron(I,sigma_z))+h[i-1]*kron(sigma_z,I)
-        H_odd_array.append(i)
+        return binary_search(mid,up,n,error)
 
-print(B_2(H_even,100))
 
+n = 10
+result_n=[]
+result_r = []
+while n<= 100:
+    global t
+    t=n
+    global h
+    h = generate_random_h(n)
+    global H_even
+    H_even = {}
+    global H_odd
+    H_odd = {}
+    global H_even_array
+    H_even_array = []
+    global H_odd_array
+    H_odd_array= []
+    for i in range(1,n+1):
+        if i%2 == 0:
+            H_even[i] = matmul(kron(sigma_x,I),kron(I,sigma_x))+matmul(kron(sigma_y,I),kron(I,sigma_y))+matmul(kron(sigma_z,I),kron(I,sigma_z))+h[i-1]*kron(sigma_z,I)
+            H_even_array.append(i)
+        else:
+            H_odd[i] = matmul(kron(sigma_x,I),kron(I,sigma_x))+matmul(kron(sigma_y,I),kron(I,sigma_y))+matmul(kron(sigma_z,I),kron(I,sigma_z))+h[i-1]*kron(sigma_z,I)
+            H_odd_array.append(i)
+
+    r = find_r(n)
+    print(n)
+    print(r)
+    result_n.append(n)
+    result_r.append(r)
+    n+= 1
+
+filename = 'last_resort_result.csv'
+savetxt(filename,c_[result_n,result_r],delimiter=',')
