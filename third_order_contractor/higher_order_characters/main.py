c26a7d30d2ee419e986fda0871345db75d828802
Shuchen Zhu <feisuxiaozhu@gmail.com>
2021-01-06 14:07:10 -0500

    conjugacy class

--- third_order_contractor/higher_order_characters/main.py
+++ third_order_contractor/higher_order_characters/main.py
@@ -2,6 +2,27 @@
 import numpy as np
 import math as math
 
+# Check whether matrix A equals matrix B
+def check_equal(A,B):
+    C = np.subtract(A,B)
+    epsilon = 0.000000001
+    sum = 0
+    for i in range(3):
+        for j in range(3):
+            sum += abs(C[i][j])
+    if sum < epsilon:
+        return True
+    else:
+        return False
+
+# Check whether matrix A exists in list matrix_list, if not return new list, o.w. return old list
+def check_whether_in_list(A,matrix_list):
+    duplicate = False    
+    for B in matrix_list:
+        if check_equal(A,B):
+            duplicate = True
+    return duplicate
+
 def convert_string_to_matrix(string):
     temp = string.split(',')
     temp1=[]
@@ -25,8 +46,27 @@ def chi_prime_2(u):
 def chi_prime_4(u):
     return np.trace(u)*np.trace(np.transpose(u.conjugate()))-1
 
+def chi_prime_8(u):
+    return 1/2*tr(u)*(tr(dag(u))**2+tr(mult(dag(u),dag(u))))-tr(dag(u))
+
 def chi_prime_9(u):
     return 1/6*(np.trace(u)*np.trace(u)*np.trace(u)+2*np.trace(np.matmul(u,np.matmul(u,u)))+3*np.trace(u)*np.trace(np.matmul(u,u)))
+
+def chi_prime_12(u):
+    first_term = -tr(u)*tr(dag(u)) + 1/4*(tr(dag(u))**2+tr(mult(dag(u),dag(u))))*(tr(u)**2+tr(mult(u,u)))
+    second_term = 1/4*(tr(u)**2*tr(dag(u))**2 + tr(u)**2*tr(mult(dag(u),dag(u))) + tr(dag(u))**2*tr(mult(u,u))+tr(mult(dag(u),dag(u)))*tr(mult(u,u)))
+    third_term = 1/6*(-tr(u)**3-3*tr(u)*tr(mult(u,u))-2*tr(mult(u,mult(u,u))))
+    fourth_term = 1/6*(tr(u)**3+3*tr(u)*tr(mult(u,u))+2*tr(mult(u,mult(u,u))))
+    fifth_term = -1/36*(tr(dag(u))**3+3*tr(dag(u))*tr(mult(dag(u),dag(u)))+2*tr(mult(dag(u),mult(dag(u),dag(u)))))*(tr(u)**3+3*tr(u)*tr(mult(u,u))+2*tr(mult(u,mult(u,u))))
+    sixth_term = 1/24*tr(dag(u))*(tr(u)**4+6*tr(u)**2*tr(mult(u,u))+3*tr(mult(u,u))**2+8*tr(u)*tr(mult(u,mult(u,u)))+6*tr(mult(u,mult(u,mult(u,u)))))
+    return first_term + second_term + third_term + fourth_term + fifth_term + sixth_term
+
+def chi_prime_13(u):
+    first_term = 1-1/4*(tr(dag(u))**2+tr(mult(dag(u),dag(u))))*(tr(u)**2+tr(mult(u,u)))
+    second_term = 1/4*(-tr(u)**2*tr(dag(u))**2-tr(u)**2*tr(mult(dag(u),dag(u)))-tr(dag(u))**2*tr(mult(u,u))-tr(mult(dag(u),dag(u)))*tr(mult(u,u)))
+    third_term = 1/3*(-tr(u)**3-3*tr(u)*tr(mult(u,u))-2*tr(mult(u,mult(u,u))))
+    fourth_term = 1/36*(tr(dag(u))**3+3*tr(dag(u))*tr(mult(dag(u),dag(u)))+2*tr(mult(dag(u),mult(dag(u),dag(u)))))*(tr(u)**3+3*tr(u)*tr(mult(u,u))+2*tr(mult(u,mult(u,u))))
+    return first_term+second_term+third_term+fourth_term
 
 def chi_prime_14(u):
     first_term = 1/6*np.trace(np.transpose(u.conjugate()))*(np.trace(u)**3+2*np.trace(np.matmul(u,np.matmul(u,u)))+3*np.trace(np.matmul(u,u))*np.trace(u))  
@@ -93,6 +133,29 @@ for matrix in result:
     category_number = find_category_number(chi_prime_2(matrix),category_2)
     category_2_result[category_number].append(matrix)
 
-for i in category_2_result['16']:
-    print(chi_prime_16(i))
+# This part checks the values in table 1 is correct in [Fly85a]
+# for i in category_2_result['2']:
+#     print(chi_prime_16(i))
+
+# This part tries to divide class 8 and class 9 into two 120=element groups
+# Pick any x\in c_8\cup c_9, check the size of the set S={uxu^{-1} | u\in s1080} is 120 
+
+print(len(result))
+repeated_set = []
+for i in range(17):
+    index = str(i+1)
+    if index != '8' and index != '9':
+        repeated_set += category_2_result[index]
+# print(len(repeated_set))
+new_set = []
+x = category_2_result['8'][1]
+for u in result:
+    y = mult(u,mult(x,dag(u)))
+    if not check_whether_in_list(y, repeated_set) and not check_whether_in_list(y,new_set):
+        new_set.append(y)
+print(len(new_set))
+
+
+
+
 
