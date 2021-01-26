b95b47454b0bc858e6f7fcc49b250a794f23df81
Shuchen Zhu <feisuxiaozhu@gmail.com>
2021-01-26 15:09:38 -0500

    d

--- third_order_contractor/higher_order_characters/match_exponential_to_expansion/main.py
+++ third_order_contractor/higher_order_characters/match_exponential_to_expansion/main.py
@@ -1,7 +1,8 @@
-# This file contain functions that check all chi's are correct
 import numpy as np
 import math as math
-import _pickle as pickle
+from numpy import linalg
+
+read_dictionary = np.load('c:/Users/feisu/Desktop/gate-complexity/third_order_contractor/higher_order_characters/result_class_element.npy',allow_pickle='TRUE').item()
 
 # Check whether matrix A equals matrix B
 def check_equal(A,B):
@@ -24,13 +25,14 @@ def check_whether_in_list(A,matrix_list):
             duplicate = True
     return duplicate
 
-def convert_string_to_matrix(string):
-    temp = string.split(',')
-    temp1=[]
-    for i in temp:
-        temp1.append(complex(i))
-    temp1=np.reshape(temp1,(3,3))
-    return temp1
+# Return the class number an S1080 element belongs
+def check_class_number(u, read_dictionary):
+    for i,matrix_list in read_dictionary.items():
+        for matrix in matrix_list:
+            if check_equal(u,matrix):
+                return i
+    return 'n/a'
+
 
 def dag(u):
     return np.transpose(u.conjugate())
@@ -41,17 +43,45 @@ def tr(u):
 def mult(a,b):
     return np.matmul(a,b)
 
+def chi_prime_1(u):
+    return 1.0
+
 def chi_prime_2(u):
     return np.trace(u)
+
+def chi_prime_3(u):
+    return chi_prime_2(dag(u))
 
 def chi_prime_4(u):
     return np.trace(u)*np.trace(np.transpose(u.conjugate()))-1
 
+def chi_prime_5(u):
+    return 1/2*(tr(mult(u,u))+tr(u)**2)
+
+def chi_prime_6(u):
+    return chi_prime_5(dag(u))
+
 def chi_prime_8(u):
     return 1/2*tr(u)*(tr(dag(u))**2+tr(mult(dag(u),dag(u))))-tr(dag(u))
 
+def chi_prime_7(u):
+    return chi_prime_8(dag(u))
+
 def chi_prime_9(u):
     return 1/6*(np.trace(u)*np.trace(u)*np.trace(u)+2*np.trace(np.matmul(u,np.matmul(u,u)))+3*np.trace(u)*np.trace(np.matmul(u,u)))
+
+def chi_prime_10(u):
+    output=[5.0,0.0,-1.0,1.0,1.0,0.0,0.0,-1.0,2.0,-1.0,-1.0,0.0,0.0,0.0,1.0,5.0,5.0]
+    class_number = check_class_number(u,read_dictionary)
+    index = int(class_number)-1
+    return output[index]
+
+def chi_prime_11(u):
+    output=[5.0,0.0,-1.0,1.0,1.0,0.0,0.0,2.0,-1.0,-1.0,-1.0,0.0,0.0,0.0,1.0,5.0,5.0]
+    class_number = check_class_number(u,read_dictionary)
+    index = int(class_number)-1
+    return output[index]
+
 
 def chi_prime_12(u):
     first_term = -tr(u)*tr(dag(u)) + 1/4*(tr(dag(u))**2+tr(mult(dag(u),dag(u))))*(tr(u)**2+tr(mult(u,u)))
@@ -90,86 +120,76 @@ def chi_prime_16(u):
     third_term = 1/2*(tr(dag(u))**2 + tr(mult(dag(u),dag(u))))
     return first_term+second_term+third_term+zero_five_term+zero_term
 
-def find_category_number(result, category_list):
-    epsilon = 0.00001
-    counter = 0
-    for i in category_list:
-        counter += 1
-        if abs(result - i) < epsilon:
-            return str(counter)
+def chi_prime_17(u):
+    return chi_prime_16(dag(u))
 
 
-file1 = open(
-    'c:/Users/feisu/Desktop/gate-complexity/third_order_contractor/higher_order_characters/data_1080.txt', 'r')
-Lines = file1.readlines()
-result = []
-for line in Lines:
-    line=line.strip('\n')
-    new_string=''
-    for char in line:
-        if char == '{':
-            new_string += ''
-        elif char == '}':
-            new_string += ''
-        elif char == '*':
-            new_string+= 'j'
-        elif char == 'I':
-            new_string+=''
-        else:
-            new_string += char
-    new_string = new_string.replace(' ','')
-    new_matrix = convert_string_to_matrix(new_string)
-    result.append(new_matrix)
+def chi_prime(r,u):
+    if r == '1':
+        return chi_prime_1(u)
+    elif r=='2':
+        return chi_prime_2(u)
+    elif r=='3':
+        return chi_prime_3(u)
+    elif r=='4':
+        return chi_prime_4(u)
+    elif r=='5':
+        return chi_prime_5(u)
+    elif r=='6':
+        return chi_prime_6(u)
+    elif r=='7':
+        return chi_prime_7(u)
+    elif r=='8':
+        return chi_prime_8(u)
+    elif r=='9':
+        return chi_prime_9(u)
+    elif r=='10':
+        return chi_prime_10(u)
+    elif r=='11':
+        return chi_prime_11(u)
+    elif r=='12':
+        return chi_prime_12(u)
+    elif r=='13':
+        return chi_prime_13(u)
+    elif r=='14':
+        return chi_prime_14(u)
+    elif r=='15':
+        return chi_prime_15(u)
+    elif r=='16':
+        return chi_prime_16(u)
+    elif r=='17':
+        return chi_prime_17(u)
 
-u1 = (1-math.sqrt(5))/2
-u2 = (1+math.sqrt(5))/2
-w = (1+math.sqrt(3)*1j)/2
-w_c = (1-math.sqrt(3)*1j)/2
+# find c_1, c_2, ..., c_17 for given i,j
+def create_vector(i,j):
+    vector = []
+    for k in range(17):
+        key = str(k+1)
+        u = read_dictionary[key][0]
+        result = chi_prime(i,u) * chi_prime(j,u)
+        vector.append([result])
+    return vector
 
-category_2 = [3,u2,1,w,w_c,-u1*w,-u1*w_c,0,0,-w_c,-w,u1,-u2*w_c,-u2*w,-1,-3*w_c,-3*w]
-category_2_result = {}
-for i in range(17):
-    category_2_result[str(i+1)]=[]
-for matrix in result:
-    category_number = find_category_number(chi_prime_2(matrix),category_2)
-    category_2_result[category_number].append(matrix)
-
-# This part checks the values in table 1 is correct in [Fly85a]
-# for i in category_2_result['2']:
-#     print(chi_prime_16(i))
-
-# This part tries to divide class 8 and class 9 into two 120=element groups
-# Pick any x\in c_8\cup c_9, check the size of the set S={uxu^{-1} | u\in s1080} is 120 
-
-
-repeated_set = []
-for i in range(17):
-    index = str(i+1)
-    if index != '8' and index != '9':
-        repeated_set += category_2_result[index]
-# print(len(repeated_set))
-new_set = []
-new_set_2 = []
-x = category_2_result['8'][1]
-for u in result:
-    y = mult(u,mult(x,dag(u)))
-    if not check_whether_in_list(y, repeated_set) and not check_whether_in_list(y,new_set):
-        new_set.append(y)
-    # elif  not check_whether_in_list(y, repeated_set) and not check_whether_in_list(y,new_set_2):
-    #     new_set_2.append(y)
-
-for u in category_2_result['8']:
-    if not check_whether_in_list(u,new_set) and not check_whether_in_list(u,new_set_2):
-        new_set_2.append(u)
-
-print(len(new_set_2))
-
-category_2_result['8'] = new_set
-category_2_result['9'] = new_set_2
+# create a matrix that we need to invert in the end
+def create_matrix():
+    matrix = []
+    for k in range(17):
+        row = []
+        key = str(k+1)
+        u = read_dictionary[key][0]
+        for ell in range(17):
+            key_2 = str(ell+1)
+            row.append(chi_prime(key_2,u))
+        matrix.append(row)
+    return matrix
+i='1'
+j='1'
+A = np.matrix(create_matrix())
+x = np.matrix(create_vector(i,j))
 
 
-with open('c:/Users/feisu/Desktop/gate-complexity/third_order_contractor/higher_order_characters/result_class_element.npy', 'wb') as f:
-    np.save(f, category_2_result)
-   
+linalg.solve(A, x) 
+
+
 
 
