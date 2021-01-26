8d8dd9bf8f31eb4fbfed8b3ff9a08ce79b050bc8
Shuchen Zhu <feisuxiaozhu@gmail.com>
2021-01-25 19:13:51 -0500

    d

---
+++ one_way_function/n_10_random/main.py
@@ -0,0 +1,73 @@
+import numpy as np
+import itertools
+import pickle
+# Set n=10, r=2, s=4, t=7
+# a helper function checks whether rank = 3
+def rank_checker(matrix):
+    row_1 = matrix[0]
+    row_2 = matrix[1]
+    row_3 = matrix[2]
+    first_sum = ((row_1+row_2)%2).tolist()[0]
+    second_sum = ((row_1+row_3)%2).tolist()[0]
+    third_sum = ((row_2+row_3)%2).tolist()[0]
+    sum_1 = sum(first_sum)
+    sum_2 = sum(second_sum)
+    sum_3 = sum(third_sum)
+    rank3 = True
+    if sum_1 == 0:
+        rank3 = False
+    elif sum_2 == 0:
+        rank3 = False
+    elif sum_3 == 0:
+        rank3 = False
+    return rank3
+
+# first generate a random 10-by-10 matrix
+# matrix = np.random.randint(2, size=(10, 10))
+
+# pick all possible 3 rows from 10 rows
+row_number = [0,1,2,3,4,5,6,7,8,9]
+row_number_set = itertools.combinations(row_number,3)
+
+# load all S matrices
+with open('./one_way_function/n_10_random/matrix_S', 'rb') as fp:
+    S_matrices = pickle.load(fp)
+
+def check_rigidity(matrix):
+    rigid = True
+    for i,j,k in row_number_set:
+        for s in S_matrices:
+            s = np.asmatrix(s)
+            row_1 = matrix[i].tolist()
+            row_2 = matrix[j].tolist()
+            row_3 = matrix[k].tolist()
+            new_matrix = [row_1,row_2,row_3]
+            new_matrix = np.asmatrix(new_matrix)
+            substracted_matrix = (new_matrix + s) % 2
+            if not rank_checker(substracted_matrix):
+                rigid = False
+                break
+        if not rigid:
+            break
+    return rigid
+
+counter = 0
+found_rigid = False
+while not found_rigid:
+    counter += 1
+    print(counter)
+    matrix = np.random.randint(2, size=(10, 10))
+    if check_rigidity(matrix):
+        print(matrix)
+        found_rigid = True
+    
+
+
+
+
+
+
+
+
+
+
--- third_order_contractor/higher_order_characters/read_result.py
+++ third_order_contractor/higher_order_characters/read_result.py
@@ -1,4 +1,21 @@
 import numpy as np
+import math as math
 
 read_dictionary = np.load('c:/Users/feisu/Desktop/gate-complexity/third_order_contractor/higher_order_characters/result_class_element.npy',allow_pickle='TRUE').item()
-print(len(read_dictionary['2']))
+def dag(u):
+    return np.transpose(u.conjugate())
+
+def tr(u):
+    return np.trace(u)
+
+def mult(a,b):
+    return np.matmul(a,b)
+
+def chi_2_m1(u):
+    return 1/2*(tr(u)**2*tr(dag(u))+tr(mult(u,u))*tr(dag(u)))-tr(u)
+
+def chi_3_1(u):
+    return 1/8*(tr(u)**4+2*tr(mult(u,u))*tr(u)**2-tr(mult(u,u))**2-2*tr(mult(u,mult(u,mult(u,u)))))
+
+u = read_dictionary['5'][0]
+print(chi_3_1(u)-chi_2_m1(u))
