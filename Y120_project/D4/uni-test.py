ad34e3e667421a9e7134f1a29820c0021c0ea5d9
Shuchen Zhu <feisuxiaozhu@gmail.com>
2021-04-01 17:33:28 -0400

    d

---
+++ Y120_project/D4/uni-test.py
@@ -0,0 +1,54 @@
+import numpy as np
+from qiskit import *
+from qiskit.extensions import *
+from qiskit.compiler import transpile
+
+q = QuantumRegister(3, 'q')
+
+U = np.array([[1, 0, 0, 0, 0, 0, 0, 0],
+              [0, 1, 0, 0, 0, 0, 0, 0],
+              [0, 0, 1, 0, 0, 0, 0, 0],
+              [0, 0, 0, 1, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 1, 0],
+              [0, 0, 0, 0, 0, 1, 0, 0],
+              [0, 0, 0, 0, 1, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 1]])
+
+qc = QuantumCircuit(q)
+
+# adjust here to set the initial state
+qc.x(q[0])
+qc.x(q[1])
+qc.x(q[2])
+
+gateU = UnitaryGate(U)
+qc.append(gateU, q)
+
+bgates=['u1','u2','u3','cx','ccx']
+print(qc)
+qc_transpiled = transpile(qc, basis_gates=bgates, optimization_level=3)
+
+# Create measurement
+meas = QuantumCircuit(3, 3)
+meas.barrier(range(3))
+meas.measure(range(3), range(3))
+
+# concatenate the mesurement with the circuit
+qc_transpiled_measure = qc + meas
+
+
+
+# Use Aer's qasm_simulator
+backend_sim = Aer.get_backend('qasm_simulator')
+
+# Execute the circuit on the qasm simulator.
+# We've set the number of repeats of the circuit
+# to be 1024, which is the default.
+job_sim = execute(qc_transpiled_measure, backend_sim, shots=1024)
+
+# Grab the results from the job.
+result_sim = job_sim.result()
+counts = result_sim.get_counts(qc_transpiled_measure)
+print(counts)
+
+
