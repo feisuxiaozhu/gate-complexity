5b73f145ca3b10dd4f89bfe1c84ead6fe9f10964
Shuchen Zhu <feisuxiaozhu@gmail.com>
2021-06-06 21:51:32 -0400

    d

--- Y120_project/D4/Qfast/uni-test.py
+++ Y120_project/D4/Qfast/uni-test.py
@@ -1,17 +1,17 @@
 import numpy as np
 from qfast import synthesize
 
-# qft4 = np.loadtxt( "matrix", dtype = np.complex128 )
+U = np.loadtxt( "U.unitary", dtype = np.complex128 )
 
-U = np.array([[1, 0, 0, 0, 0, 0, 0, 0],
-              [0, 1, 0, 0, 0, 0, 0, 0],
-              [0, 0, 1, 0, 0, 0, 0, 0],
-              [0, 0, 0, 1, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 1, 0],
-              [0, 0, 0, 0, 0, 1, 0, 0],
-              [0, 0, 0, 0, 1, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 1]], dtype=np.complex128)
-# U.astype(np.complex64)
+# U = np.array([[1, 0, 0, 0, 0, 0, 0, 0],
+#               [0, 1, 0, 0, 0, 0, 0, 0],
+#               [0, 0, 1, 0, 0, 0, 0, 0],
+#               [0, 0, 0, 1, 0, 0, 0, 0],
+#               [0, 0, 0, 0, 0, 0, 1, 0],
+#               [0, 0, 0, 0, 0, 1, 0, 0],
+#               [0, 0, 0, 0, 1, 0, 0, 0],
+#               [0, 0, 0, 0, 0, 0, 0, 1]], dtype=np.complex128)
+
 
 # print(U)
 print(synthesize( U ) )
