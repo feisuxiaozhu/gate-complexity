ffe7112373379e1a0c00f0d85aa901e5d3ef2ec8
Shuchen Zhu <feisuxiaozhu@gmail.com>
2021-08-05 16:15:33 -0400

    third order

--- third_order_contractor/higher_order_characters/match_exponential_to_expansion/re_exponentiation_3rd_order.py
+++ third_order_contractor/higher_order_characters/match_exponential_to_expansion/re_exponentiation_3rd_order.py
@@ -20,15 +20,40 @@ for i in range(cutoff):
         key = str(i+1)
         key_2 = str(j+1)
         combo_key = key+ '_'+key_2
-        if key == key_2:
-            coefficient_dict[combo_key] = -1/2* coefficient_dict[key]**2
+        if key == '1' and key_2 == '1':
+            coefficient_dict[combo_key] = -1/2 * (coefficient_dict[key]-1)**2
+        elif key == '1':
+            coefficient_dict[combo_key] = -1/2 * \
+                (coefficient_dict[key]-1)*coefficient_dict[key_2]
+        elif key_2 == '1':
+            coefficient_dict[combo_key] = -1/2 * \
+                coefficient_dict[key]*(coefficient_dict[key_2]-1)
         else:
-            coefficient_dict[combo_key] = -1/2*coefficient_dict[key]*coefficient_dict[key_2]
+            coefficient_dict[combo_key] = -1/2 * \
+                coefficient_dict[key]*coefficient_dict[key_2]
 for i in range(cutoff):
     for j in range(cutoff):
         for k in range(cutoff):
             combo_key = str(i+1) + '_' + str(j+1) + '_' + str(k+1)
-
+            key = str(i+1)
+            key_2 = str(j+1)
+            key_3 = str(k+1)
+            if (key == '1' and key_2 == '1') and key_3 =='1': 
+                coefficient_dict[combo_key] = 1/3 * (coefficient_dict[key]-1)**3
+            elif key == '1' and key_2 == '1':
+                coefficient_dict[combo_key] = 1/3 * (coefficient_dict[key]-1)*(coefficient_dict[key_2]-1)*coefficient_dict[key_3]
+            elif key == '1' and key_3 == '1':
+                coefficient_dict[combo_key] = 1/3 * (coefficient_dict[key]-1)*(coefficient_dict[key_3]-1)*coefficient_dict[key_2]
+            elif key_3 == '1' and key_2 == '1':
+                coefficient_dict[combo_key] = 1/3 * (coefficient_dict[key_2]-1)*(coefficient_dict[key_3]-1)*coefficient_dict[key]
+            elif key == '1':
+                coefficient_dict[combo_key] = 1/3 * (coefficient_dict[key]-1)*coefficient_dict[key_2]*coefficient_dict[key_3]
+            elif key_2 == '1':
+                coefficient_dict[combo_key] = 1/3 * coefficient_dict[key]*(coefficient_dict[key_2]-1)*coefficient_dict[key_3]
+            elif key_3 == '1':
+                coefficient_dict[combo_key] = 1/3 * coefficient_dict[key]*coefficient_dict[key_2]*(coefficient_dict[key_3]-1)
+            else:
+                coefficient_dict[combo_key] = 1/3 * coefficient_dict[key]*coefficient_dict[key_2]*coefficient_dict[key_3]
 
 
 # Find each chi_i*chi_j's coefficients, and add them to corresponding chi_i's.
@@ -42,6 +67,20 @@ for i in range(cutoff):
         for k in range(cutoff):
             key_3 = str(k+1)
             coefficient_dict[key_3] += simplification_vector[k]*constant
+
+for i in range(cutoff):
+    for j in range(cutoff):
+        for k in range(cutoff):
+            combo_key = str(i+1) + '_' + str(j+1) + '_' + str(k+1)
+            key = str(i+1)
+            key_2 = str(j+1)
+            key_3 = str(k+1)
+            simplification_vector = read_dictionary_2[combo_key]
+            constant = coefficient_dict[combo_key]
+            for l in range(cutoff):
+                key_4 = str(l+1)
+                coefficient_dict[key_4] += simplification_vector[l]*constant
+
         
 # print result 
 for i in range(cutoff):
