c3b4927bab4fa7aa77e12a836b4356b3147379e3
Shuchen Zhu <feisuxiaozhu@gmail.com>
2021-11-23 14:57:57 -0500

    d

--- Y120_project/S108/group_generator.py
+++ Y120_project/S108/group_generator.py
@@ -4,24 +4,56 @@ import _pickle as pickle
 import math as math
 from numpy.linalg import matrix_power
 
+# Check whether matrix A equals matrix B
+
+
+def check_equal(A, B):
+    C = np.subtract(A, B)
+    epsilon = 0.000000001
+    sum = 0
+    for i in range(len(A)):
+        for j in range(len(A)):
+            sum += abs(C[i][j])
+    if sum < epsilon:
+        return True
+    else:
+        return False
+
+
+def mult(a, b):
+    return np.matmul(a, b)
+
+# Return the key of matrix A in dic
+
+
+def check_dict_key(A, dic):
+    for key, value in dic.items():
+        if check_equal(value, A):
+            return key
+
+
+def g(p, q, r, s, t):
+    return w**p * \
+        mult(mult(matrix_power(C, q), matrix_power(E, r)), matrix_power(V, 2*s+t))
+
+
 w = math.cos(2*math.pi/3) + 1j*math.sin(2*math.pi/3)
 C = np.array([[1, 0, 0], [0, w, 0], [0, 0, w**2]])
-E = np.array([[0, 1, 0], [1, 0, 0], [0, 0, 1]])
+E = np.array([[0, 1, 0], [0, 0, 1], [1, 0, 0]])
 V = 1/(1j*math.sqrt(3)) * np.array([[1, 1, 1], [1, w, w**2], [1, w**2, w]])
 
-result = []
+S108 = []
+S108_dict = {}
+
 
 for p in range(3):
     for q in range(3):
         for r in range(3):
             for s in range(2):
                 for t in range(2):
-                    matrix = w**p * \
-                        matrix_power(C, q) * matrix_power(E, r) * \
-                        matrix_power(V, 2*s+t)
-                    result.append(matrix)
-
-with open('./S108_element.npy', 'wb') as f:
-    np.save(f, result)
-
+                    matrix = g(p, q, r, s, t)
+                    S108 .append(matrix)
+                    key = str(p)+str(q)+str(r)+str(s)+str(t)
+                    S108_dict[key] = matrix
+                
 
