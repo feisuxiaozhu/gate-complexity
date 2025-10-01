13467fe8e874e58cfbd0fe97ee3c1f28f7c01310
Shuchen Zhu <feisuxiaozhu@gmail.com>
2025-10-01 13:51:02 -0400

    d

--- H_learning/plot_analysis_2_qubit.py
+++ H_learning/plot_analysis_2_qubit.py
@@ -7,6 +7,12 @@ T_total = [2.213e+05,1.815e+06,2.220e+07,2.704e+08]
 l2_error= [9.456e-05,1.001e-05, 8.747e-07,7.505e-08]
 
 
+# corrected prefactor, N_shots = 25 and varies, repeat = 200, success rate >90, eps start from 1e-2
+# np.array([0.1,0.2,0.3, 0.5,0.6,0.3,0.2,0.1,0.1, 0.2,0.1,0.1, 0.3,0.22,0.15])
+T_total = [1.175e+06,1.466e+07,1.307e+08,1.599e+09,1.947e+10]
+l2_error= [ 1.282e-03,9.061e-05, 1.095e-05, 8.480e-07,7.552e-08]
+
+
 T_total = np.array(T_total)
 l2_error = np.array(l2_error)
 
